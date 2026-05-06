3ddd202208f1b8e039049db64bd0e5be19a16520
delfinagamenara <delfigamenara@gmail.com>
2026-05-06 14:31:27 -0300

    Initial commit

---
+++ Assets/Scripts/CajaCollision.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CajaCollision : MonoBehaviour
+{
+    void OnCollisionEnter (Collision col){
+        if (col.gameObject.CompareTag ("Caja"))
+        {
+            col.gameObject.GetComponent <CajaSpawner>().LocateBoxAtRandomPosition ();
+        }
+    }
+}
